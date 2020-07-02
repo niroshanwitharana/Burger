b4ae4c5962d51e4154422ff82939d298c0d3d3a6
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-07-02 10:34:02 +0100

    DAtabase table edited

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@
 CREATE SCHEMA `burgers_db` ;
 
 -- Create a table--
-CREATE TABLE `burgers_db`.`new_table` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `burger_name` VARCHAR(45) NULL,
-  `devoured` TINYINT(1) DEFAULT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(45) NULL,
+  devoured TINYINT(1) DEFAULT NULL,
+  PRIMARY KEY (id));
 
