4d402447a502a6934bc181a6a4dc0bc2b136d7fc
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-07-02 10:32:52 +0100

    heroku DB setup

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,6 @@
 var mysql = require("mysql");
+const config = require('./config');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection = mysql.createConnection(config.db.URL);
 
 module.exports = connection;
